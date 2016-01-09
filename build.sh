8277f5a01d58804aaae8cd35b63d1f19b1b919e9
Himanshu Khurana <himanshukhurana@Himanshus-MacBook-Pro.local>
2016-01-09 15:42:01 +0400

    Removed protoc installation steps

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 echo "Installing Protoc"
 
-cd /tmp
-git clone https://github.com/google/protobuf google-protobuf
-cd google-protobuf
-./autogen.sh
-./configure --prefix=/local/software/package/protobuf
-make
-make check
-make install
 
 
 echo "Starting Build ..."
