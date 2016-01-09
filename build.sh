17d29017a0a708b808f2b93ecd8ca97e366d2b6f
Himanshu Khurana <himanshukhurana@Himanshus-MacBook-Pro.local>
2016-01-09 15:34:13 +0400

    Added protoc instawqllation steps

--- build.sh
+++ build.sh
@@ -1,2 +1,16 @@
+echo "Installing Protoc"
+
+cd /tmp
+git clone https://github.com/google/protobuf google-protobuf
+cd google-protobuf
+./autogen.sh
+./configure --prefix=/local/software/package/protobuf
+make
+make check
+make install
+
+
 echo "Starting Build ..."
 mvn clean install -DskipTests
+echo "Finished Build ..."
+
